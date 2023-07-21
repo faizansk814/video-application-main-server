9bb8be6243f8404a3415d6d5ddee8f443a2dc171
faizansk <faizansk814@gmail.com>
2023-07-21 19:53:53 +0530

    ok

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(cors())
 app.use("/user",userRouter)
 app.use("/auth",authrouter)
+
+app.get("/start",(req,res)=>{
+    res.status(200).send({msg:"start the server"})
+})
 app.listen(8080,async ()=>{
     try {
         await connection
